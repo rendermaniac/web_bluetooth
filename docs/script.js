29fd38eae3d0d9df0e9966991f2757462ad2b426
Simon Bunker <simon@rendermania.com>
2023-03-09 22:05:51 +1100

    wip

--- docs/script.js
+++ docs/script.js
@@ -5,13 +5,10 @@ button.addEventListener("click", BLEManager);
 
 async function BLEManager() {
 
-        // Request the Bluetooth device through browser
-        let manufacturerData = [{
-        companyIdentifier: 0x00e0, /* Google */
-        dataPrefix: new Uint8Array([0x01, 0x02]),
-        }];
         const device = await navigator.bluetooth.requestDevice({
-                filters: [{ manufacturerData }],
+                filters: [{
+    services: ["0x68"]
+  }],
                 optionalServices: ["battery_service", "device_information"],
         });
 }
