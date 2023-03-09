fa295a619b3aa0b0fb27970e9a0732a3b77c7bc4
Simon Bunker <simon@rendermania.com>
2023-03-09 21:46:31 +1100

    wip

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,7 @@
 const button = document.getElementById("getDetails");
 const details = document.getElementById("details");
 
-button.addEventListener("click", BLEManager)
+button.addEventListener("click", BLEManager);
 
 async function BLEManager() {
 
@@ -11,51 +11,5 @@ async function BLEManager() {
                 {name: "Pixel 6 Pro"}
                 ],
                 optionalServices: ["battery_service", "device_information"],
-        });
-
-        // Connect to the GATT server
-        // We also get the name of the Bluetooth device here
-        let deviceName = device.gatt.device.name;
-        const server = await device.gatt.connect();
-
-        // Getting the services we mentioned before through GATT server
-        const batteryService = await server.getPrimaryService("battery_service");
-        const infoService = await server.getPrimaryService("device_information");
-
-        // Getting the current battery level
-        const batteryLevelCharacteristic = await batteryService.getCharacteristic("battery_level");
-
-        // Convert recieved buffer to number
-        const batteryLevel = await batteryLevelCharacteristic.readValue();
-        const batteryPercent = await batteryLevel.getUint8(0);
-
-        // Getting device information
-        // We will get all characteristics from device_information
-        const infoCharacteristics = await infoService.getCharacteristics();
-        console.log(infoCharacteristics);
-        let infoValues = [];
-
-        const promise = new Promise((resolve, reject) => {
-            infoCharacteristics.forEach(async (characteristic, index, array) => {
-                // Returns a buffer
-                const value = await characteristic.readValue();
-                console.log(new TextDecoder().decode(value));
-                // Convert the buffer to string
-                infoValues.push(new TextDecoder().decode(value));
-                if (index === array.length - 1) resolve();
-            });
-        });
-
-        promise.then(() => {
-            // Display all the information on the screen
-            // use innerHTML
-            details.innerHTML = `
-            Device Name - ${deviceName}<br />
-            Battery Level - ${batteryPercent}%<br />
-            Device Information:
-            <ul>
-            ${infoValues.map((value) => `<li>${value}</li>`).join("")}
-            </ul> 
-            `;
         });
 }
